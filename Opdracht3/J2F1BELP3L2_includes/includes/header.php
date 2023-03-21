ae5e44ebe1b410150be9e5ba1b294f0c4328e399
Mielesgames <emieltlam11@gmail.com>
2023-03-21 13:57:31 +0100

    why do I always have to commit twice

--- Opdracht3/J2F1BELP3L2_includes/includes/header.php
+++ Opdracht3/J2F1BELP3L2_includes/includes/header.php
@@ -3,7 +3,8 @@ Gebruik hier tenminste een header afbeelding en een menu
 Zorg dat je in het menu bij elk item een url parameter zet
 om te bepalen welke inhoud er ingeladen moet worden in je html
 -->
-<div>
+<div id="headerHolder">
+    <h1>Super accurate news website</h1>
     <a href="?page=onderwerp1.php">oh no page 1</a>
     <a href="?page=onderwerp2.php">is page 2 better?</a>
     <a href="?page=onderwerp3.php">this is awesome</a>
